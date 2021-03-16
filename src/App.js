e2294a37734601febf5b0d0ad73bbd6e2bfad584
Mridul2820 <immridulpanda2820@gmail.com>
2021-03-16 16:00:09 +0530

    key and url update

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import SearchBar from './components/SearchBar'
 import './styles/app.scss'
 
 const App = () => {
+    const apiURL =  'http://www.omdbapi.com/'
+    const apiKey = `?apikey=${process.env.REACT_APP_API_KEY}`
+
     return (
         <div className="app">
             <header>
